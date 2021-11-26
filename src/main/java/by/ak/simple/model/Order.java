4383dc6e6358bc443324d473c97de61d4940e6e4
Alexander Karankevich <alexander.karankevich@gmail.com>
2021-11-27 00:10:41 +0300

    add domain model (Customer&Order) add lombok externalize version info to gradle.properties

---
+++ src/main/java/by/ak/simple/model/Order.java
@@ -0,0 +1,24 @@
+package by.ak.simple.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders") // to avoid naming conflicts
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String sku;
+}
